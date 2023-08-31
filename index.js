088bfe1f55c19adf898737608327df1ce5f0c01d
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-31 20:12:57 +0200

    obsługa streama

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
 
-let readStream = fs.createReadStream("./data/text.txt");
+let readStream = fs.createReadStream("./data/text.txt", {
+  highWaterMark: 16 * 1024,
+});
 
 readStream.on("data", function (chunk) {
   console.log(`Wielkość chunka: ${chunk.length / 1024} KB`);
 
   console.log(chunk);
+});
+
+readStream.on("end", function () {
+  console.log("Zakonczony stream");
 });
